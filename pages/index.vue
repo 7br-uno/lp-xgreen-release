959919eec464de7d4505d9a1b84e24ff1b97a572
7br-uno <bruno7p@live.com>
2021-05-25 16:33:04 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1 class="example">{{ hello_world }}</h1>
+    <v-btn
+      elevation="2"
+      color="blue"
+      @click="hello_daniel()"
+      dark
+    >Click me</v-btn>
+  </div>
+</template>
+
+<script>
+  export default{
+    data: () => {
+      return {
+        hello_world: 'Hello World',
+        daniel_name: 'Daniel'
+      }
+    },
+    mounted(){
+      this.hello_daniel();
+    },
+    methods:{
+      hello_daniel(){
+        console.log('Hello ' + this.daniel_name);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.example{
+  color: red;
+}
+</style>
