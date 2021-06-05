c9bac3805d47f936807043209554386b60e69063
Bruno Neves <dev@7br.uno>
2021-06-05 15:22:28 -0300

    Graph

--- components/index/sixth_banner.vue
+++ components/index/sixth_banner.vue
@@ -1,11 +1,152 @@
 <template>
-  <section class="wrapper"></section>
+  <section class="wrapper">
+    <v-container>
+      <div
+        :class="$vuetify.breakpoint.mdAndUp ? 'display-2' : 'display-1'"
+        class="mt-15 pt-10 primary--text font-weight-black text-center overing text-uppercase"
+      >
+        Rendimento desde 2008
+      </div>
+      <v-row class="white--text graph-div mb-16">
+        <v-col cols="12" md="3" :order="$vuetify.breakpoint.mdAndUp ? 'last' : ''">
+          <div class="grey d-flex align-center text-center rounded-lg pa-3 mt-10">
+            <v-icon color="white" left>
+              mdi-download
+            </v-icon>
+            <span class="caption lh-12">
+            Baixe nossos resultados agora!
+            </span>
+          </div>
+          <div class="black text-center rounded-lg pa-3 mt-3">
+            <div class="font-weight-bold">
+              Rendimento anual X-GREEN
+            </div>
+            <div class="font-weight-bold ml-4">
+              2018 <span class="yellow--text ml-2">+ 506%*</span>
+            </div>
+            <div class="font-weight-bold ml-4">
+              2019 <span class="yellow--text ml-2">+ 808%*</span>
+            </div>
+            <div class="font-weight-bold ml-4">
+              2020 <span class="yellow--text ml-2">+ 936%*</span>
+            </div>
+            <div class="font-weight-bold ml-4">
+              2021 <span class="blue--text darken-1 ml-2">+ 276%**</span>
+            </div>
+            <div class="caption text-left px-3 mt-1">Estratégia arrojada</div>
+          </div>
+          <div class="caption text-center mt-3">
+            <div>
+              * Maio até dezembro
+            </div>
+            <div class="blue--text darken-1">
+              ** Até o final de março
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="12" md="9" :order="$vuetify.breakpoint.mdAndUp ? 'first' : ''">
+          <!--https://www.chartjs.org/docs/latest/charts/line.html-->
+          <line-chart :chart-data="datacollection" :options="chartOptions" class="graph mx-auto my-0 my-md-10"></line-chart>
+        </v-col>
+      </v-row>
+      <div v-if="!$vuetify.breakpoint.mdAndUp" style="height: 280px"></div>
+      <div class="text-center white--text font-weight-bold headline mb-15 pt-10 text-uppercase">
+        Lembre-se: Você vai poder copiar a nossa estratégia e lucrar mais
+      </div>
+    </v-container>
+    <div class="text-center">
+      <v-btn
+        x-large
+        color="pink"
+        dark
+        class="desloc_vert"
+      >
+        <b>QUERO LUCRAR AGORA</b>
+      </v-btn>
+    </div>
+  </section>
 </template>
+
+<script>
+import LineChart from '@/components/LineChart'
+export default {
+  components: {
+    LineChart
+  },
+  data () {
+    return {
+      datacollection: null,
+      chartOptions:{
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  },
+  mounted () {
+    this.fillData()
+  },
+  methods: {
+    fillData () {
+      this.datacollection = {
+        labels: ['2017', '2018', '2019', '2020', '2021'],
+        datasets: [
+          {
+            label: 'X-GREEN',
+            tension: 0,
+            fill: false,
+            borderColor: '#00ff00',
+            borderWidth: 6,
+            data: [15, 60, 80, 100, 60]
+          },
+          {
+            label: 'Bitcoin',
+            tension: 0,
+            fill: false,
+            borderColor: '#b000ff',
+            borderWidth: 5,
+            data: [13, 30, 45, 44, 30],
+          },
+          {
+            label: 'Ibovespa',
+            tension: 0,
+            fill: false,
+            borderColor: '#00f1ea',
+            borderWidth: 5,
+            data: [10, 25, 30, 15, 9],
+          },
+          {
+            label: 'Poupança',
+            tension: 0,
+            fill: false,
+            borderColor: '#ff8600',
+            borderWidth: 5,
+            data: [10, 15, 13, 14, 10],
+          },
+        ]
+      }
+    }
+  }
+}
+</script>
+
 <style scoped lang="scss">
 .wrapper {
   background: #1F1F1F;
-  padding: 100px;
-  text-align: center;
-  height: 80vh;
+}
+.graph-div{
+  height: 500px;
+  margin-bottom: 50px;
+}
+.graph {
+  max-width: 800px;
+  background: #070707;
+  border-radius: 10px;
+  padding: 40px;
+}
+.desloc_vert{
+  margin-bottom: -28px;
+}
+.lh-12{
+  line-height: 1.1;
 }
 </style>
